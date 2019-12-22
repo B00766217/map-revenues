f83cefa02ae1c297b0097b769b011a7af2824e8b
sileB <44704661+B00766217@users.noreply.github.com>
2019-12-22 09:54:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,57 @@
 # choropleth map of revenues
+import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 import plotly.graph_objs as go
 
-import pandas as pd
-#app = dash.Dash(__name__)
-#server = app.server
-df = pd.read_csv("df_map_group_sum.csv")
 
-#app.layout = html.Div([
-fig = go.Figure(data=go.Choropleth(
-    locations = df['Territory'],
-    z = df['2020B_amount'],
-    text = df['Territory_name'],
-    colorscale = 'Greens',
-    autocolorscale=False,
-    reversescale=False,
-    marker_line_color='darkgray',
-    marker_line_width=0.8,
-    colorbar_tickprefix = '$',
-    colorbar_title = 'Revenue<br>EUR',
-))
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_with_codes.csv')
 
-fig.update_layout(
-    title_text='SBKE Group - Map of 2020 Proposed Budgeted Revenues',
-    geo=dict(
-        showframe=True,
-        showcoastlines=False,
-        projection_type='equirectangular'
-    ),
-),
 
-fig.show()
-#if __name__ == '__main__':
-    #app.run_server(debug=True)
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+
+app.layout = html.Div([html.Div([html.H1("Demographic Data by Country")],
+                                style={'textAlign': "center", "padding-bottom": "30"}),
+                       html.Div([html.Span("Metric to display : ", className="six columns",
+                                           style={"text-align": "right", "width": "40%", "padding-top": 10}),
+                                 dcc.Dropdown(id="value-selected", value='lifeExp',
+                                              options=[{'label': "Population ", 'value': 'pop'},
+                                                       {'label': "GDP Per Capita ", 'value': 'gdpPercap'},
+                                                       {'label': "Life Expectancy ", 'value': 'lifeExp'}],
+                                              style={"display": "block", "margin-left": "auto", "margin-right": "auto",
+                                                     "width": "70%"},
+                                              className="six columns")], className="row"),
+                       dcc.Graph(id="my-graph")
+                       ], className="container")
+
+
+@app.callback(
+    dash.dependencies.Output("my-graph", "figure"),
+    [dash.dependencies.Input("value-selected", "value")]
+)
+def update_figure(selected):
+    dff = df.groupby(['iso_alpha', 'country']).mean().reset_index()
+    def title(text):
+        if text == "pop":
+            return "Population (million)"
+        elif text == "gdpPercap":
+            return "GDP Per Capita (USD)"
+        else:
+            return "Life Expectancy (Years)"
+    trace = go.Choropleth(locations=dff['iso_alpha'],z=dff[selected],text=dff['country'],autocolorscale=False,
+                          colorscale="YlGnBu",marker={'line': {'color': 'rgb(180,180,180)','width': 0.5}},
+                          colorbar={"thickness": 10,"len": 0.3,"x": 0.9,"y": 0.7,
+                                    'title': {"text": title(selected), "side": "bottom"}})
+    return {"data": [trace],
+            "layout": go.Layout(title=title(selected),height=800,geo={'showframe': False,'showcoastlines': False,
+                                                                      'projection': {'type': "miller"}})}
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
